ae18287b44556b06504213fc9dc63cd635e426bf
naveenvivek <modernnaveen@gmail.com>
2024-02-18 10:04:02 +0800

    1st
    1st

---
+++ src/test/java/com/naveen/ec2/Ec2javatestApplicationTests.java
@@ -0,0 +1,11 @@
+package com.naveen.ec2;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2javatestApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
